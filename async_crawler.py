01670f1483838983e43392dab7f1c2d8eb904fc6
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2026-03-03 15:22:41 +0200

    Add async handling methods to AsyncCrawler

--- async_crawler.py
+++ async_crawler.py
@@ -3,11 +3,25 @@ import aiohttp
 
 
 class AsyncCrawler:
-    def __init__(self, base_url, page_data, max_concurrency):
+    def __init__(self, base_url, base_domain, page_data, max_concurrency):
         self.base_url = base_url
-        # self.base_domain # revitään beis_urlista
+        self.base_domain = base_domain
         self.page_data = page_data
         self.lock = asyncio.Lock()
         self.max_concurrency = max_concurrency
         self.semaphore = asyncio.Semaphore(self.max_concurrency)
+        self.session
+
+    async def __aenter__(self):
         self.session = aiohttp.ClientSession()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.close()
+
+    async def add_page_visit(self, normalized_url):
+        async with self.lock:
+            if normalized_url in self.page_data.keys():
+                return True
+            else:
+                return False
